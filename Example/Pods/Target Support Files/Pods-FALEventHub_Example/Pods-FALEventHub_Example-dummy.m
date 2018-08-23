7cd92ac1eeda3c91e29c0ba38bb4649054f6667b
Javier González Ovalle <jagonzalezo@falabella.cl>
2018-08-23 12:48:46 -0300

    Template

---
+++ Example/Pods/Target Support Files/Pods-FALEventHub_Example/Pods-FALEventHub_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FALEventHub_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FALEventHub_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-FALEventHub_Tests/Pods-FALEventHub_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FALEventHub_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_FALEventHub_Tests
+@end

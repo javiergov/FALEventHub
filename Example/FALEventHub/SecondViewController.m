e629871387180a3f2650e484696a194331535ab3
Javier González Ovalle <jagonzalezo@falabella.cl>
2018-08-27 11:31:55 -0300

    Example for Objective-C with the same message shared between ViewControllers.

--- Example/FALEventHub/SecondViewController.m
+++ Example/FALEventHub/SecondViewController.m
@@ -15,38 +15,68 @@
 @end
 
 static NSString *const message = @"Event Message";
+static NSString *const infoKey = @"exampleValue";
+static NSInteger const infoValue = 777;
 
 @implementation SecondViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self.view setBackgroundColor:[UIColor lightGrayColor]];
-
-    EventHubBlock swiftClosure = ^(NSDictionary *messageInfo){
-        [self simpleFunction: messageInfo];
-    };
-    NSString *subsId = [EventHub subscribeWithInstance:self
-                                                      forEvent:message
-                                                         async:YES thenCall:swiftClosure];
-    NSLog(@"EventHub subscribeWithInstance returns an NSString id: %@", subsId);
-    
-    //it can also be discarded.
-    [EventHub subscribeWithInstance:self
-                           forEvent:message
-                              async:NO
-                           thenCall:^(NSDictionary *messageInfo) {
-                               NSLog(@"block direclty implemented in the subscribe method. %@", messageInfo.description);
-                           }];
 }
 
 - (void)simpleFunction:(NSDictionary<NSString *, id>*)infoDictionary {
-    NSLog(@"•SecondVC: simple function from Event: %@", infoDictionary.description);
+    NSLog(@"•SecondVC: simple method from Event: %@", infoDictionary.description);
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - IBAction
+
+- (IBAction)exampleButtonAction:(UIButton *)sender {
+    
+    switch ([sender tag]) {
+            
+            case 1:
+        {
+            NSString *idString =
+            [EventHub subscribeWithInstance:self
+                                   forEvent:message
+                                      async:NO
+                                   thenCall:^(NSDictionary *messageInfo) {
+                                       NSLog(@"•SecondVC: block direclty implemented in the subscribe method. %@", messageInfo.description);
+                                   }];
+            NSLog(@"•SecondVC: Subscribed to %@", idString);
+        }
+            break;
+            
+            case 2:
+        {
+            EventHubBlock swiftClosure = ^(NSDictionary *messageInfo) { [self simpleFunction: messageInfo]; };
+            [EventHub subscribeWithInstance:self
+                                   forEvent:message
+                                      async:YES thenCall:swiftClosure];
+        }
+            break;
+            
+            case 3:
+            [EventHub unsubscribeWithInstance:self fromEvent:message];
+            break;
+            
+            case 4:
+            [EventHub triggerWithEventMessage:message optionalInfo:nil];
+            break;
+            
+            case 5:
+        {
+            NSNumber *infoNumber = [NSNumber numberWithInt:infoValue];
+            NSDictionary *messageInfo = @{infoKey : infoNumber};
+            [EventHub triggerWithEventMessage:message optionalInfo:messageInfo];
+        }
+            break;
+            
+        default:
+            NSLog(@"•SecondVC: button not tagged");
+            break;
+    }
+    
 }
 
 @end
